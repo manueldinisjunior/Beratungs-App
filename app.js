9a142564f54788edece2ed9e5c0ffed619ca11ca
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-10-30 12:46:56 +0200

    Application

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
-"use strict";
+const adviceBtn = document.getElementById("adviceBtn");
+const adviceNum = document.getElementById("adviceNum");
+const adviceText = document.getElementById("adviceText");
+const card = document.getElementById("card");
+const endpoint = "https://api.adviceslip.com/advice";
+let clicked = false;
 
-const diceBtn = document.querySelector(".button-section");
-const adviceNumber = document.querySelector(".advice-number");
-const adviceText = document.querySelector(".quotes");
-const apiUrl = "https://api.adviceslip.com/advice";
+function getAdvice() {
+  startLoading();
 
-//Button click event
-diceBtn.addEventListener("click", () => {
-  //Request Data
-  fetch(apiUrl, { cache: "no-cache" })
+  fetch(endpoint)
     .then((response) => response.json())
-    .then((response) => {
-      let data = response.slip;
-      let dataId = data.id;
-      let dataAdvice = data.advice;
-      //Inject to DOM
-      adviceNumber.innerHTML = `advice # ${dataId}`;
-      adviceText.innerHTML = dataAdvice;
-    }).catch(error => console.error(error));
-});
+    .then((advice) => {
+      adviceNum.innerText = advice.slip.id;
+      adviceText.innerText = advice.slip.advice;
+    })
+    .catch((err) => console.log(err))
+    .finally(() => finishLoading());
+}
+
+function startLoading() {
+  card.style.opacity = 0;
+}
+
+function finishLoading() {
+  card.style.opacity = 1;
+}
+
+function needAdvice() {
+  if (clicked) {
+    startLoading();
+
+    setTimeout(() => {
+      adviceNum.innerText = "0";
+      adviceText.innerText = "Did you actually read the previous advice?";
+
+      finishLoading();
+    }, 500);
+  } else {
+    clicked = true;
+
+    getAdvice();
+
+    setTimeout(() => {
+      clicked = false;
+    }, 5000);
+  }
+}
+
+window.onload = () => getAdvice();
+
+adviceBtn.addEventListener("click", needAdvice);
